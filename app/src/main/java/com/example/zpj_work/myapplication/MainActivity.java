fafa93016e44f97026947ce591fdaa96a259afa1
zpjgit <zpj.love@foxmail.com>
2019-09-27 09:46:19 +0800

    修改 MainActivity.java 通过端点 使用 bulkTransfer 收发数据,收到的数据是全0; 修改 content_main.xml 将 我筛选的设备以及收到的数据通过 setText 显示在 TextView上.

--- app/src/main/java/com/example/zpj_work/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zpj_work/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbEndpoint;
 import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbRequest;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,19 +20,21 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private static final String TAG                = "hik";
-    private static final int VendorID              = 0x1ef5;
-    private static final int ProductID             = 0x0200;
+    private static final String TAG                   = "hik";
+    private static final int VendorID                 = 0x1ef5;
+    private static final int ProductID                = 0x0200;
     private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
-    byte[] mybuffer;
+    private static final int TIMEOUT                  = 3000;
     private UsbManager myUsbManager;
     private TextView info;
     private UsbDevice myUsbDevice;
@@ -39,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     private UsbDeviceConnection myDeviceConnection;
     private UsbEndpoint epOut;
     private UsbEndpoint epIn;
+//    byte[] mybuffer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +52,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mybuffer = new byte[]{(byte)0x02, (byte)0x00, (byte)0x06, (byte)0x00, (byte)0x05, (byte)0xcc, (byte)0xcc, (byte)0xcc, (byte)0xcc, (byte)0x03, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00};
-        Log.i("mybuffer", bytesToHexString(mybuffer));
+//        5A 55 06 00 0D 06 00 C8 6A 69
+//        mybuffer = new byte[]{(byte)0x5A, (byte)0x55, (byte)0x06, (byte)0x00, (byte)0x0D, (byte)0x06, (byte)0x00, (byte)0xC8, (byte)0x6A, (byte)0x69};
+
+//        cmd = "5A5506000D0700C96A69";
+//        cmd01 = "5A5508000D11000000D56A69";
+//        buf = HexToByteArr(cmd01);
+
+//        Log.i(TAG, bytesToHexString(mybuffer));
         info = (TextView) findViewById(R.id.info);
         myUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
         enumerateDevice();
@@ -61,6 +72,21 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //添加一个按钮用来再次枚举设备
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                enumerateDevice();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
     //枚举设备
@@ -75,9 +101,9 @@ public class MainActivity extends AppCompatActivity {
             for (UsbDevice device : deviceList.values()) {
                 sb.append(device).toString();
                 sb.append("\n");
-                info.setText(sb);
+//                info.setText(sb);
                 //输出设备信息
-                Log.d(TAG, "DeviceInfo: "+device.getDeviceId() + ", "+device.getProductId());
+                Log.d(TAG, "DeviceInfo: "+device.getDeviceId() + ", "+device.getVendorId()+", "+device.getProductId());
 
                 //枚举到设备
                 if (device.getVendorId() == VendorID && device.getProductId() == ProductID) {
@@ -108,20 +134,31 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "interfaceCounts : "+ myUsbDevice.getInterfaceCount());
         for (int i=0; i<myUsbDevice.getInterfaceCount(); i++) {
+            Log.d(TAG, "interfaceCounts too : "+ myUsbDevice.getInterfaceCount());
+
             UsbInterface intf = myUsbDevice.getInterface(i);
+
             //根据手上的设备做一些判断，其实这些信息都可以在枚举到设备时打印出来
-            if (intf.getInterfaceClass() == 3 && intf.getInterfaceSubclass() == 0 && intf.getInterfaceProtocol() == 0) {
+            if (intf.getInterfaceClass() == 255 && intf.getInterfaceSubclass() == 0 && intf.getInterfaceProtocol() == 255) {
                 myInterface = intf;
-                Log.d(TAG, "找到我的设备接口");
+                Log.d(TAG, "找到我的设备接口. 接口的id号: "+intf.getId());
                 openDevice();
             }
+//            myInterface = intf;
+//            Log.d(TAG, "找到我的设备接口 "+intf.getInterfaceClass()+", "+intf.getInterfaceSubclass()+", "+intf.getInterfaceProtocol());
+//            openDevice();
+
             break;
         }
     }
 
     //获取权限, 打开设备
     private void openDevice() {
+        Log.d(TAG, "openDevice : "+ myUsbDevice.getInterfaceCount());
+
         if (myInterface != null) {
+            Log.d(TAG, "openDevice 不为空 : "+ myUsbDevice.getInterfaceCount());
+
             UsbDeviceConnection conn = null;
             //在open前判断是否有连接权限；对于连接权限可以静态分配，也可以动态分配权限
             PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
@@ -158,14 +195,79 @@ public class MainActivity extends AppCompatActivity {
             epIn = myInterface.getEndpoint(0);
         }
         info.setText(myUsbDevice.getDeviceName() + "\nInterfaceCount: " + myUsbDevice.getInterfaceCount() + "\nEndpointCount: " + myInterface.getEndpointCount());
+        Log.d(TAG, "assignEndpoint: \n"+myUsbDevice.getDeviceName() + "\nInterfaceCount: " + myUsbDevice.getInterfaceCount() + "\nEndpointCount: " + myInterface.getEndpointCount());
         Log.d(TAG, "assignEndpoint");
 
-        int re = myDeviceConnection.bulkTransfer(epOut, mybuffer, mybuffer.length, 3000);
+        String cmd = "5A5506000D0700C96A69";
+        String cmd01 = "5A5508000D11000000D56A69";
+        byte[] buf = HexToByteArr(cmd01);
+//        int re = myDeviceConnection.bulkTransfer(epOut, buf, buf.length, 3000);
+        int re = send_Message(buf);
+//        int re = myDeviceConnection.bulkTransfer(epOut, mybuffer, mybuffer.length, 3000);
         byte[] reByte = new byte[64];
-        int re2 = myDeviceConnection.bulkTransfer(epIn, reByte, reByte.length, 3000);
-        Log.i("reByte", "re"+re2+"\n"+bytesToHexString(reByte));
-        Toast.makeText(this, bytesToHexString(reByte), Toast.LENGTH_LONG).show();
+//        int re2 = myDeviceConnection.bulkTransfer(epIn, reByte, reByte.length, 3000);
+        int re2 = receive_Message(reByte);
+//        for (Byte byte1:reByte) {
+//            System.err.println(byte1);
+//        }
 
+        //-----------------------------------------
+//        int outMax = outEndpoint.getMaxPacketSize();
+//        int inMax = inEndpoint.getMaxPacketSize();
+//        ByteBuffer byteBuffer = ByteBuffer.allocate(inMax);
+//        UsbRequest usbRequest = new UsbRequest();
+//        usbRequest.initialize(connection, inEndpoint);
+//        usbRequest.queue(byteBuffer, inMax);
+//        if(connection.requestWait() == usbRequest) {
+//            byte[] retData = byteBuffer.array();
+//            for (Byte byte1 : retData) {
+//                System.err.println(byte1);
+//            }
+//        }
+        //-----------------------------------------
+
+        Log.i(TAG, "re"+re2+"\n"+bytesToHexString(reByte));
+        Toast.makeText(this, bytesToHexString(reByte), Toast.LENGTH_LONG).show();
+        info.setText(myUsbDevice.getDeviceName() + "\nInterfaceCount: " + myUsbDevice.getInterfaceCount() + "\nEndpointCount: " + myInterface.getEndpointCount()
+                +"\nepOut: "+cmd01
+                +"\nre"+re2+"\n"+bytesToHexString(reByte)
+        );
+
+    }
+
+    /*发送数据*/
+    public int send_Message(byte[] sendBytes){
+        int ret = -1;
+        if(epOut != null){
+            ret = myDeviceConnection.bulkTransfer(epOut, sendBytes, sendBytes.length, TIMEOUT);
+            Log.d(TAG,"write ok");
+        }else {
+            Log.d(TAG,"write failed");
+        }
+
+        return ret;
+
+    }
+
+    /*接收数据*/
+    public  int receive_Message(byte[] receiveBytes){
+
+        int ret = -1;
+        if(epIn != null){
+            ret = myDeviceConnection.bulkTransfer(epIn, receiveBytes, receiveBytes.length, TIMEOUT);
+//            ret = Byte2Hex(myDeviceConnection.bulkTransfer(epIn, receiveBytes, receiveBytes.length, TIMEOUT));
+            Log.d(TAG, "read ok");
+
+        }else {
+            Log.d(TAG, "read failed");
+        }
+
+        return ret;
+    }
+
+    static public String Byte2Hex(Byte inByte)//1字节转2个Hex字符
+    {
+        return String.format("%02x", inByte).toUpperCase();
     }
 
     public String bytesToHexString(byte[] src) {
@@ -182,6 +284,37 @@ public class MainActivity extends AppCompatActivity {
             stringBuilder.append(hv);
         }
         return stringBuilder.toString();
+    }
+
+    static public int isOdd(int num)
+    {
+        return num & 0x1;
+    }
+
+    static public byte HexToByte(String inHex)
+    {
+        return (byte) Integer.parseInt(inHex,16);
+    }
+
+    static public byte[] HexToByteArr(String inHex)
+    {
+        int hexlen = inHex.length();
+        byte[] result;
+        if (isOdd(hexlen)==1)
+        {//奇数
+            hexlen++;
+            result = new byte[(hexlen/2)];
+            inHex="0"+inHex;
+        }else {//偶数
+            result = new byte[(hexlen/2)];
+        }
+        int j=0;
+        for (int i = 0; i < hexlen; i+=2)
+        {
+            result[j]=HexToByte(inHex.substring(i,i+2));
+            j++;
+        }
+        return result;
     }
 
     @Override
